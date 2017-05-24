0ed32c0e1f025f5c01acb2d8b0e463cdf2f966af
SimiPro <sihuber@gmx.net>
2017-05-24 09:44:37 +0200

    Update ipython_config.py

--- ipython_config.py
+++ ipython_config.py
@@ -1,2 +1,12 @@
+# execute first: this will create this file on your system. then add these two lines
+# ipython profile create dev 
+
+# you can select a profile like this: ipython --profile=foo        # start IPython using the new profile
+# so add it afterwards like this in your emacs file: 
+#(setq
+ #  python-shell-interpreter "ipython"
+  # python-shell-interpreter-args "--profile=dev"
+#)
+
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']
